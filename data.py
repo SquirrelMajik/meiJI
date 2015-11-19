ec2c7ee95b2ec242be8ed05646b0dfd0d6f906de
majik <mumu296979387@gmail.com>
2015-11-19 16:48:44 +0800

    decision tree

---
+++ data.py
@@ -0,0 +1,92 @@
+import copy
+
+CATEGORY = 'category'
+
+
+class Data:
+    def __init__(self, **arg):
+        if CATEGORY not in arg:
+            raise Exception("data must have category")
+        self.category = arg.pop(CATEGORY)
+        self.data = arg
+        
+    def __str__(self):
+        data_str = [value for _, value in self.data]
+        data_str.append(self.category)
+        data_str = '\t'.join(data_str)
+        return data_str
+        
+    def __len__(self):
+        return len(self.data)
+        
+    def __getitem__(self, key):
+        return self.data.get(key)
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __delitem__(self, key):
+        del self.data[key]
+        
+    def get_labels(self):
+        return set([label for label, _ in self.data])
+            
+            
+class DataSet:
+    def __init__(self, dataset=[], labels=[]):
+        if not isinstance(dataset, list):
+            raise Exception("dataset must be list")
+        if not isinstance(dataset, list):
+            raise Exception("labels must be list")
+        if dataset:
+            if not labels:
+                labels = range(len(dataset[0]))
+            if len(dataset[0]) == len(labels):
+                self._category_label = labels[-1]
+                labels = labels[:-1]
+            elif len(dataset[0]) == len(labels) + 1:
+                self._category_label = ''
+            else:
+                raise Exception("labels don't suit dataset")
+        self.dataset = [_create_data(labels, data) for data in dataset]
+        self.labels = set(labels)
+        
+    def __iter__(self):
+        return iter(self.dataset)
+        
+    def __str__(self):
+        label_str = '\t'.join(self.labels + [self._category_label])
+        dataset_str = '\n'.join([data.__str__() for data in dataset])
+        dataset_str = label_str + dataset_str
+        return dataset_str
+    
+    def __len__(self):
+        return len(self.dataset)
+        
+    def append(self, data):
+        if not data.get_labels() == self.labels:
+            raise Exception("append incorrect data into dataset")
+        self.dataset.append(data)
+        return self
+        
+    def saltshaker(self, label, value ,split=True):
+        dataset = DataSet()
+        temp_dataset = [copy.deepcopy(data) for data in self.dataset \
+            if data[label] == value]
+        labels = set(self.labels)
+        if split:
+            for data in temp_dataset:
+                del data[label]
+            labels -= set([label])
+        dataset.dataset = temp_dataset
+        dataset.labels = labels
+        return dataset
+        
+        
+def _create_data(labels, data):
+    category = data[-1]
+    data = data[:-1]
+    common_data = {label:d for label, d in zip(labels, data)}
+    common_data[CATEGORY] = category
+    return Data(**common_data)
+    
--- decision_tree.py
+++ decision_tree.py
@@ -1,57 +1,35 @@
 from __future__ import division
 
+from collections import Counter
 from math import log
+from data import DataSet
+import json
 
-
-class Data:
-    def __init__(self, labels, *arg):
-        self.category = arg[-1]
-        if not labels:
-            labels = range(len(arg))
-        self.data = {label: value for label,value in zip(labels, arg[:-1])}
-        
-    def __str__(self):
-        category = "category:{}".format(self.category)
-        data_str = ["{0}:{1}".format(label, value) \
-            for label,value in self.data.items()]
-        data_str.append(category)
-        data_str = '\n'.join(data_str)
-        return data_str
-            
-
-def create_dataset(dataset, labels=[]):
-    return [Data(labels, *data) for data in dataset]
+    
+# def create_dataset(dataset, labels=[]):
+#     return [Data(labels, *data) for data in dataset]
     
 
 def calculate_shannon_entropy(dataset):
-    lenth = len(dataset)
-    labels = {}
-    for data in dataset:
-        if data.category not in labels:
-            labels[data.category] = 0
-        labels[data.category] += 1
+    countor = Counter([data.category for data in dataset])
     shannon_entropy = 0
-    for _, count in labels.items():
-        prob = count/lenth
+    for _, count in countor.items():
+        prob = count/len(dataset)
         shannon_entropy -= prob * log(prob, 2)
     return shannon_entropy
     
-
-def saltshaker(dataset, label, value):
-    return [data for data in dataset if data.data[label] == value]
-
-
-def _get_labels(dataset):
-    return [label for label in dataset[0].data]
+    
+# def _get_labels(dataset):
+#     return [label for label in dataset[0].data]
     
     
 def _feature(dataset):
     base_entropy = calculate_shannon_entropy(dataset)
-    for label in _get_labels(dataset):
-        unique_values = set([data.data[label] for data in dataset])
+    for label in dataset.labels:
+        unique_values = set([data[label] for data in dataset])
         entropy = 0
         for value in unique_values:
-            temp_set = saltshaker(dataset, label, value)
+            temp_set = dataset.saltshaker(label, value)
             prob = len(temp_set) / len(dataset)
             entropy -= prob * calculate_shannon_entropy(temp_set)
         gain = base_entropy - entropy
@@ -60,12 +38,26 @@ def _feature(dataset):
     
 def gains(dataset):
     feature_result = [(label, gain) for label, gain in _feature(dataset)]
-    feature_result.sort(lambda x,y : int(x[1]-y[1]))
+    feature_result.sort(lambda x,y : -1 if x[1] > y[1] else 1)
     return feature_result
     
     
-def gain_first(dataset):
-    return gains(dataset)[0]
+def gain_best(dataset):
+    return gains(dataset)[0][0]
+    
+
+def tree(dataset):
+    counter = Counter([data.category for data in dataset])
+    if len(counter) == 1:
+        return counter.most_common()[0][0]
+    best_label = gain_best(dataset)
+    decision_tree = {best_label: {}}
+    
+    unique_values = set([data[best_label] for data in dataset])
+    for value in unique_values:
+        decision_tree[best_label][value] = tree(
+            dataset.saltshaker(best_label, value))
+    return decision_tree
     
     
 if __name__ == "__main__":
@@ -77,6 +69,6 @@ if __name__ == "__main__":
         [4, 50, 3, 'bad'],
         [2, 30, 2, 'bad']
     ]
-    dataset = create_dataset(dataset, labels)
-    feature = gain_first(dataset)
-    print feature
+    dataset = DataSet(dataset, labels)
+    d_tree = tree(dataset)
+    print json.dumps(d_tree, indent=1)
