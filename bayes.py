cd97f19a843179e1f54e74aa7ff620c1c21b28f0
majik <mumu296979387@gmail.com>
2015-11-20 17:32:16 +0800

    xxx

--- bayes.py
+++ bayes.py
@@ -1,16 +1,24 @@
 
 from data import DataSet
+import re
+
+
+def split_words(string):
+    pattern = re.compile(r"(?!:[a-zA-Z]+)|([0-9]+(\.[0-9]+)?)")
+    return re.findall(pattern, string)
+    
 
 
 if __name__ == "__main__":
-    labels = ['string']
-    dataset = [
-        [1, 100, 3, 'good'],
-        [2, 50, 2, 'good'],
-        [4, 120, 3, 'good'],
-        [4, 50, 3, 'bad'],
-        [2, 30, 2, 'bad']
-    ]
-    dataset = DataSet(dataset, labels)
-    d_tree = tree(dataset)
+    # labels = ['string']
+    # dataset = [
+    #     [1, 100, 3, 'good'],
+    #     [2, 50, 2, 'good'],
+    #     [4, 120, 3, 'good'],
+    #     [4, 50, 3, 'bad'],
+    #     [2, 30, 2, 'bad']
+    # ]
+    # dataset = DataSet(dataset, labels)
+    # d_tree = tree(dataset)
+    print split_words("qwe 234 asg qwer 234 sadf 324gfas fawe3 wefetqwt")
     
