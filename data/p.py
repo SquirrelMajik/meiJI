6ddc2ad8bef3d825bbba321967b61503be797e0f
majik <mumu296979387@gmail.com>
2015-12-15 14:51:13 +0800

    fix

--- data/p.py
+++ data/p.py
@@ -34,34 +34,53 @@ from __future__ import division
     
     
 def P(dataset, string):
-    def _p(dataset, condition):
+    def _p_c(dataset, condition):
         count = 0
         for data in dataset:
             if dataset[data, condition[0]] == condition[1]
                 count += 1
         return count / len(dataset)
         
-    def _p_under(dataset, condition, under):
+    def _p_c_u(dataset, condition, under):
         dataset = dataset.shake({under[0]: under[1]})
-        return _p(dataset, condition[0], condition[1])
+        return _p_c(dataset, condition[0], condition[1])
         
-    match = re.match(r'(?:(\w+=\w+))(?:\|((?:\w+=\w+)(?:\*(?:\w+=\w+))*))',
-                     string)
+    def _p_cs(dataset, conditions):
+        return mul([_p_c(dataset, condition) for condition in conditions])
+        
+    def _p_cs_u(dataset, conditions, under):
+        return mul([_p_c_u(dataset, condition, under) \
+            for condition in conditions])
+            
+    def _p_c_us(dataset, condition, unders):
+        return _p_cs_u(dataset, unders, condition) * _p_c(dataset, condition) \
+            / mul(_p_c(under) for under in unders)
+            
+    def _p_cs_us(dataset, conditions, unders):
+        if not unders:
+            return _p_cs(conditions)
+        return mul(_p_c_us(dataset, condition, unders) \
+            for condition in conditions)
+            
+    reg = r'(?:(\w+=\w+)(?:\*(?:\w+=\w+))*))' + \
+          r'(?:\|((?:\w+=\w+)(?:\*(?:\w+=\w+))*))'
+    match = re.match(reg, string)
     assert match
-    condition, unders = match.groups()
-    if not unders:
-        return _p(dataset, condition)
-    unders = unders.split('*')
-    if len(under) == 1:
-        under = unders[0]
-        return _p_under(dataset, condition, under)
-    else:
-        p_under = 1
-        p = 1
-        for under in unders:
-            p_under *= _p_under(dataset, condition, under)
-            p *= _p(dataset, under)
-        return _p(dataset, condition) * p_under / p
+    conditions, unders = match.groups()
+    return _p_cs_us(dataset, conditions, unders)
+    # if not unders:
+    #     return _p(dataset, condition)
+    # unders = unders.split('*')
+    # if len(under) == 1:
+    #     under = unders[0]
+    #     return _p_under(dataset, condition, under)
+    # else:
+    #     p_under = 1
+    #     p = 1
+    #     for under in unders:
+    #         p_under *= _p_under(dataset, condition, under)
+    #         p *= _p(dataset, under)
+    #     return _p(dataset, condition) * p_under / p
         
     # if '|' in string:
     #     under, condition = string.split('|')
@@ -74,6 +93,5 @@ def P(dataset, string):
     #     return _p(dataset, condition[0], condition[1])
         
         
-        
-P(dataset, under(a=1), b=2)
-dataset.P("a=1|b=2")
+def mul(l):
+    reduce(lambda x,y: x*y, l, 1)
